e3458e8b797532abbc56a1048013d97790676aa4
midastown <mehdihachimi1@gmail.com>
2023-08-12 22:50:34 +0000

    integrating alembic & sqlite

--- models.py
+++ models.py
@@ -4,10 +4,14 @@ from main import db
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
     requests = db.relationship("ModelRequest", backref='user')
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
 
     def set_password(self, value):
         """Store the password as a hash for security."""
@@ -21,19 +25,24 @@ class User(db.Model):
 
 
 class AiModels(db.Model):
+    __tablename__ = 'ai_models'
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String, unique=True, nullable=False)
     requestsPM = db.Column(db.Integer, nullable=False)
     tokenPM = db.Column(db.Integer, nullable=False)
     pricePerThousandToken = db.Column(db.Integer, nullable=False)
-    models = db.relationship('ModelRequest', backref='aimodels', uselist=False)
+    models = db.relationship('ModelRequest', backref='ai_models', uselist=False)
+
+    def __repr__(self):
+        return f'<Model: {self.model}>, <RequestsPerMin: {self.requestsPM}>, <TokenPM: {self.tokenPM}>, <Price: {self.pricePerThousandToken}>'
 
 
 class ModelRequest(db.Model):
+    __tablename__ = 'model_request'
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String)
     answer = db.Column(db.String)
     cost = db.Column(db.Integer) # Integer (in cents) since sqlite doesn't like decimals
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    model_id = db.Column(db.Integer, db.ForeignKey('aimodels.id'))
+    model_id = db.Column(db.Integer, db.ForeignKey('ai_models.id'))
 
